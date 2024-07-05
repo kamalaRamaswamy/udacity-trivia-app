db2b1525c4725b2e2e57dbbcc4bbbac6951d38f0
Kamala <kamala.ramaswamyk@gmail.com>
2024-07-05 14:33:28 +0530

    Tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -11,25 +11,76 @@ class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
     def setUp(self):
-        """Define test variables and initialize app."""
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        
-        self.app = create_app({
-            "SQLALCHEMY_DATABASE_URI": self.database_path
-        })
-
+        self.app = create_app()
         self.client = self.app.test_client
+        self.database_name = "trivia_test"
+        self.database_path = "postgres://{}/{}".format(
+            'localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
 
-    
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            self.db.create_all()
+
     def tearDown(self):
-        """Executed after reach test"""
         pass
-
     """
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['categories']))
+    
+    def test_page_not_found(self):
+        res = self.client().get('/questions?page=1000', json={'difficulty': 1})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource Not Found')
+    
+    def test_get_questions(self):
+        """" gets questions using /questions endpoint without a page argument"""
+        res = self.client().get('/questions?page=2')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(len(data['categories']))
+
+    def test_add_question(self):
+        new_question = {
+            'question': 'quiz question',
+            'answer': 'abc',
+            'difficulty': 1,
+            'category': 1
+        }
+        total_questions_before = len(Question.query.all())
+        res = self.client().post('/questions', json=new_question)
+        data = json.loads(res.data)
+        total_questions_after = len(Question.query.all())
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertEqual(total_questions_after, total_questions_before + 1)
+
+    def test_search_questions(self):
+        new_search = {'searchTerm': 'a'}
+        res = self.client().post('/questions/search', json=new_search)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertIsNotNone(data['questions'])
+        self.assertIsNotNone(data['total_questions'])
 
 
 # Make the tests conveniently executable
